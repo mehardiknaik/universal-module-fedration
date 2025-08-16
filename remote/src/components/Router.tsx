8690bacf0dfff2fbb746fa3cd0d1f3ff6096f23c
mehardiknaik <hardiknaik7444@gmail.com>
2025-08-16 19:09:24 +0530

    Update Router.tsx

--- remote/src/components/Router.tsx
+++ remote/src/components/Router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router';
 import HomePage from '../pages/HomePage';
 import Header from './Header';
 import AboutPage from '../pages/AboutPage';
@@ -7,14 +7,14 @@ import { remote } from './Remote';
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Header />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
